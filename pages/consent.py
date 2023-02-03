8edbb295bb1735ff65583b7c99bd82d3b6201027
Maurício dos Passos <mpassos@ucs.br>
2023-02-03 13:48:22 -0300

    add consent page to app layout

---
+++ pages/consent.py
@@ -0,0 +1,28 @@
+import dash
+import dash_bootstrap_components as dbc
+from dash import html
+
+dash.register_page(__name__, path="/", name="BlaConsent", title="Ethical Dilemmas in Software Development")
+
+consentTitle = html.Section(
+            [
+              html.H2("Online Study Consent Form"),
+              html.P("Assista o vídeo a seguir e clique em Concordar...")
+            ]
+)
+
+viFrame = html.Iframe(
+              width="700", height="394",
+              src="https://www.youtube.com/embed/P5HNeahRYDM",
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share; fullscreen" ,
+              )
+
+videoFrame = html.Div(viFrame, className="videoFrame")
+
+
+layout = dbc.Container(
+      [
+        consentTitle,
+        videoFrame,
+      ]
+)
