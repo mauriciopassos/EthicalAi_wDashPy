ff7dc7ab8c48019a36b392de755e676aa006c5f4
Maurício dos Passos <mpassos@ucs.br>
2023-02-06 09:45:46 -0300

    add badge span

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from dash import html
 
 app = dash.Dash(__name__,
                 meta_tags=[{'name': 'viewport', 'content': 'width=device-width, initial-scale=1.0, maximum-scale=1.2, minimum-scale=0.5,'}],
-                    external_stylesheets=['https://fonts.googleapis.com/css2?family=Share+Tech&display=swap',dbc.themes.BOOTSTRAP],
+                    external_stylesheets=['https://fonts.googleapis.com/css2?family=Share+Tech&display=swap',dbc.themes.BOOTSTRAP,dbc.icons.BOOTSTRAP],
                     use_pages=True)
 
 app.title = "Ethical Dilemmas in Software Development"
@@ -12,9 +12,24 @@ app.title = "Ethical Dilemmas in Software Development"
 title = html.H1("Ethical Dilemmas in Software Development")
 header = html.Header(title)
 
+badges = html.Span(
+        [
+            html.Div(
+                [
+                    dbc.Badge([html.I(className="bi bi-bank"), "  AIRES at PUCRS"], href="https://en.airespucrs.org/",
+                        className="text-decoration-none", style={'margin-right': '5px'}),
+                    dbc.Badge([html.I(className="bi bi-filetype-py"), "  Made with Python"], href="https://www.python.org/",
+                        className="text-decoration-none", style={'margin-right': '5px'}),
+                    dbc.Badge([html.I(className="bi bi-github"), "  Nkluge-correa"], href="https://nkluge-correa.github.io/",
+                        className="text-decoration-none", style={'margin-right': '5px'})
+                ], style={'textAlign': 'center', 'margin-bottom': '20px'})
+        ]
+)
+
 app.layout = dbc.Container(
     [
         header,
+        badges,
 
         dash.page_container
     ]
